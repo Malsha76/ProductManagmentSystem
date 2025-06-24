f0012c4403775b604884cf6c63bb24b4f41b9aca
Malsha Ekanayake <121780234+Malsha76@users.noreply.github.com>
2025-06-24 16:57:53 +0530

    productService

---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,43 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public Product saveProduct(Product product) {
+        return repository.save(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    public Product getProductById(String id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public String deleteProduct(String id) {
+        repository.deleteById(id);
+        return "Product deleted: " + id;
+    }
+
+    public Product updateProduct(Product product) {
+        Product existingProduct = repository.findById(product.getId()).orElse(null);
+        if (existingProduct != null) {
+            existingProduct.setName(product.getName());
+            existingProduct.setDescription(product.getDescription());
+            existingProduct.setPrice(product.getPrice());
+            return repository.save(existingProduct);
+        }
+        return null;
+    }
+}
